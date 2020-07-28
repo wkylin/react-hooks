049e2dd0b9bff1e50b128918c643a36768f4ea54
wkylin <wkylin.w@gmail.com>
2020-07-29 01:08:30 +0800

    useCallback...master

--- src/components/ReactSmartInRender/Parent3.js
+++ src/components/ReactSmartInRender/Parent3.js
@@ -28,42 +28,43 @@ const Child2 = memo(({ num, sayHiInChildren }) => {
     </div>);
 }, areEqual);
 
-// const Parent = () => {
-//   console.log('Parent rendered');
-//
-//   let [count, setCount] = useState(0);
-//   const handleMouseEnter = () => setCount(prevCount => prevCount + 1);
-//   const sayHiInChildren = child => console.log(`hi~~~~~~~~~ - from ${child}`);
-//   // const sayHiInChildren = useCallback(child => console.log(`hi~~~~~~~~~ — from ${child}`), [])
-//
-//   return (
-//     <div onMouseEnter={handleMouseEnter} style={{ border: '1px solid black', padding: '50px' }}>
-//       Parent. Count: {count}
-//       <Child1 num={1} sayHiInChildren={sayHiInChildren} />
-//       <Child2 num={2} sayHiInChildren={sayHiInChildren} />
-//     </div>
-//   );
-// };
+const Parent = () => {
+  console.log('Parent rendered');
 
-class Parent extends React.Component {
-  state={
-    count: 0
-  }
-  
-  sayHiInChildren = child => console.log(`hi~~~~~~~~~ - from ${child}`);
-  
-  handleMouseEnter = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  
-  render() {
-    return <div onMouseEnter={this.handleMouseEnter} style = {{border: '1px solid black', padding: '50px'}}>
-      Parent. Count: {this.state.count}
-      <Child1 num={1} sayHiInChildren={this.sayHiInChildren}/>
-      <Child2 num={2} sayHiInChildren={this.sayHiInChildren}/>
+  let [count, setCount] = useState(0);
+  const handleMouseEnter = () => setCount(prevCount => prevCount + 1);
+  const sayHiInChildren = child => console.log(`hi~~~~~~~~~ - from ${child}`);
+  // const sayHiInChildren = useCallback(child => console.log(`hi~~~~~~~~~ — from ${child}`), [])
+
+  return (
+    <div onMouseEnter={handleMouseEnter} style={{ border: '1px solid black', padding: '50px' }}>
+      Parent. Count: {count}
+      <Child1 num={1} sayHiInChildren={sayHiInChildren} />
+      <Child2 num={2} sayHiInChildren={sayHiInChildren} />
     </div>
-  }
-}
+  );
+};
+
+// class Parent extends React.Component {
+//   state={
+//     count: 0
+//   }
+//
+//   sayHiInChildren = child => console.log(`hi~~~~~~~~~ - from ${child}`);
+//
+//   handleMouseEnter = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+//
+//   render() {
+//     return <div onMouseEnter={this.handleMouseEnter} style = {{border: '1px solid black', padding: '50px'}}>
+//       Parent. Count: {this.state.count}
+//       <Child1 num={1} sayHiInChildren={this.sayHiInChildren}/>
+//       <Child2 num={2} sayHiInChildren={this.sayHiInChildren}/>
+//     </div>
+//   }
+// }
+
 export default Parent;
