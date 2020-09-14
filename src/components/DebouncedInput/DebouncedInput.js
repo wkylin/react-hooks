b0087203799379bd2a62e51a29a1b670c2205890
wkylin <wkylin.w@gmail.com>
2020-09-14 21:18:40 +0800

    debounce input

--- src/components/DebouncedInput/DebouncedInput.js
+++ src/components/DebouncedInput/DebouncedInput.js
@@ -1,50 +1,71 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback } from 'react'
 
 function useDebouncedValue(initialVal, delay, callback) {
   // state to store value of the input
-  const [val, setValue] = useState(initialVal);
+  const [val, setValue] = useState(initialVal)
   // state to store the timer
-  const [timer, setTimer] = useState(null);
+  const [timer, setTimer] = useState(null)
 
   // function to be called on every change of input
   const handleInputChange = useCallback(
-    event => {
+    (event) => {
       // assigning the value to a local variable
       // Or you can make the event persist if you want to pass around the event obj
-      const inputVal = event.target.value;
+      const inputVal = event.target.value
       // setting the value for immediate use
-      setValue(inputVal);
+      setValue(inputVal)
       // the same old code checking & clearing if a callback is already scheduled
       if (timer) {
-        clearTimeout(timer);
+        clearTimeout(timer)
       }
       // setting a new callback to execute
-      const timerId = setTimeout(() => callback(inputVal), delay);
-      setTimer(timerId);
+      const timerId = setTimeout(() => callback(inputVal), delay)
+      setTimer(timerId)
 
       // if components unmounts when there is a scheduled callback
       // then clearing out the callback
       return () => {
         if (timer) {
-          clearTimeout(timer);
+          clearTimeout(timer)
         }
-      };
+      }
     },
     [callback, delay, timer]
-  );
+  )
 
   // returning the val and the debounceChange which can be assigned
   // to a input onchange handler
 
-  return [val, handleInputChange];
+  return [val, handleInputChange]
 }
 
-export default function InputComp({ onDebouncedValChange, delay, placeholder }) {
+export default function InputComp({
+  onDebouncedValChange,
+  delay,
+  placeholder,
+}) {
   const [val, handleInputChange] = useDebouncedValue(
-    "",
+    '',
     delay,
     onDebouncedValChange
-  );
+  )
 
-  return <input type="text" placeholder={placeholder} value={val} onChange={handleInputChange} />;
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={val}
+      onChange={handleInputChange}
+    />
+  )
+}
+
+{
+  /* <InputComp
+  onDebouncedValChange={(val) => {
+    console.log(val)
+  }}
+  delay={500}
+  placeholder="hello"
+/> */
 }
